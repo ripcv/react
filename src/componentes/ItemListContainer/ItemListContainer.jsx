176f1d1ef624bffc8d94aa9a6d977bd6c4608fdf
Ripcv <gefallene.engel@gmail.com>
2023-12-19 00:39:08 -0300

    se crea lista de productos incluido el filtrado por categoria

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { getExchangeRate } from "../../services/exchangeapi"
+import { useGetCategories } from "../../hooks/useCategory"
 import "./ItemListContainer.css"
+import CategoryContainer from "../CategoryContainer/CategoryContainer"
 
 const ItemListContainer = ({ productsData }) => {
   const [usd, setUsd] = useState(0);
-
+  const { categorieData } = useGetCategories();
+  
   useEffect(()=> {
     getExchangeRate()
     .then((res) => {
@@ -16,9 +19,11 @@ const ItemListContainer = ({ productsData }) => {
     }, []);
 
 
+ 
 return (
 
   <div className="tarjetas">
+  <CategoryContainer categorieData={categorieData}/>
     <div className="producto" >
       {
         productsData.map(product => {
