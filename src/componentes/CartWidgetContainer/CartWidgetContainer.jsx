6c1f7311cccc21c44542982b35d0d23f479c445f
Ripcv <gefallene.engel@gmail.com>
2023-12-19 17:16:02 -0300

    Se corrige uso de FontAwsome

--- src/componentes/CartWidgetContainer/CartWidgetContainer.jsx
+++ src/componentes/CartWidgetContainer/CartWidgetContainer.jsx
@@ -1,13 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
-const carrito = <FontAwesomeIcon icon={faCartShopping} />
 
 function CartWidgetContainer() {
   return (
     <div id="carrito" className="carrito">
     <div className="hide" id="mensaje"></div>
-    <a href="./carrito.html"><div>{carrito} 0</div></a>
+    <a href="./carrito.html"><div><FontAwesomeIcon icon={faCartShopping} /> 0</div></a>
   </div>
   )
 }
