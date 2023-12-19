dd16f0643f8b2f996f49e6e06a34c5590eae5fec
Ripcv <gefallene.engel@gmail.com>
2023-12-19 17:35:07 -0300

    Ajustes visuales

--- src/pages/Producto.jsx
+++ src/pages/Producto.jsx
@@ -4,7 +4,9 @@ import { Link, useParams } from "react-router-dom";
 import './Producto.css'
 import ItemCount from "../componentes/ItemCount/ItemCount";
 
-/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/* 
+posible futura funcionalidad
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons"; */
 
 
@@ -25,17 +27,21 @@ const Producto = () => {
                 return (
                     <div key={product.id} className="single-product" >
                     <img className="product-imagen"src={`../../src/assets/images/tienda/${product.img}`} alt="" />
-                  {/*   <div className="control-product">
+                  {/*   
+                    posible futura funcionalidad
+                  <div className="control-product">
                     <Link to={`/tienda/producto/${product.id-1}`}><FontAwesomeIcon icon={faArrowLeft} /></Link>
                     <Link to={`/tienda/producto/${product.id+1}`}><FontAwesomeIcon icon={faArrowRight} /> </Link>
                     </div> */}
+                    <div>
+                    <div className="titulo__formato titulo-producto"><h1>{product.nombreProducto}</h1></div>
                     <div className="product-content">
-                    <h1>{product.nombreProducto}</h1>
                     <p>{product.descripcion}</p>
                     <span>Precio: {Math.floor(product.valor/usd)} USD - ${product.valor} CLP. </span>
                     <div className="product-select">
                     <ItemCount product={product}/>
                     <button type="button" className="boton-principal btn-product">Comprar</button>
+                    </div>
                     </div>
                 </div>
                   </div>
