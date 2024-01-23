b06b0c9f568f2cc5fb9a5bb127ba75c4985965b0
Ripcv <gefallene.engel@gmail.com>
2024-01-23 16:48:24 -0300

    Se agrega simulacion de finalizar compra

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,33 +1,58 @@
-
-import ProductsCarContainer from '../componentes/ProductsCarContainer/ProductsCarContainer'
 import './Checkout.css'
+import ProductsCarContainer from '../componentes/ProductsCarContainer/ProductsCarContainer'
+import { useContext, useState } from 'react';
+import { CartContext } from '../context/CartContext';
+import { useNavigate } from 'react-router-dom';
+
+
 
 
 const Checkout = () => {
+   const { productosCarrito, setCarrito } = useContext(CartContext);
+    const [nombre, setNombre] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const history = useNavigate();
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (productosCarrito.length === 0){
+            alert("Ingrese Productos al Carrito")
+            
+        }else if(nombre && direccion && correo && telefono) {
+            alert("Compra Exitosa.")
+            setCarrito([]);
+            history('/tienda')
+        }
+        else {
+            alert("Ingrese los datos del comprador.")
+          
+        }
+    };
+
+   
     return (
         <div className="checkout-page">
-        <h1 className="titulo__formato">Carrito de Compras</h1>
-        <section className="contenido contenido-carrito">
-        <form action="" className="formFinalizarCompra">
-            <div className="datos-comprador" id="datos-comprador">
-                <h3>Datos Comprador</h3>
-                <label htmlFor="nombreUsuario">Nombre</label>
-                <input type="text" className="input-text" name="nombreUsuario" id="nombreUsuario"/>
-                <label htmlFor="direccionUsuario">Direccion Envio</label>
-                <input type="text" className="input-text" name="direccionUsuario" id="direccionUsuario" placeholder="Ingresa la direccion"/>
-                <label htmlFor="correoUsuario">Correo</label>
-                <input type="text" className="input-text" name="correoUsuario" id="correoUsuario"/>
-                <label htmlFor="telefonoContacto">Telefono</label>
-                <input type="text" className="input-text" name="telefonoContacto" id="telefonoContacto"/>
-          
-            </div>
+            <h1 className="titulo__formato">Carrito de Compras</h1>
+            <section className="contenido contenido-carrito">
+                <form onSubmit={handleSubmit} className="formFinalizarCompra">
+                    <div className="datos-comprador" id="datos-comprador">
+                        <h3>Datos Comprador</h3>
+                        <label htmlFor="nombreUsuario">Nombre</label>
+                        <input type="text" className="input-text" name="nombreUsuario" value={nombre} onChange={(e) => setNombre(e.target.value)} />
 
-        <ProductsCarContainer/>
-        </form>
-        </section>
+                        <label htmlFor="direccionUsuario">Direccion Envio</label>
+                        <input type="text" className="input-text" name="direccionUsuario" id="direccionUsuario" placeholder="Ingresa la direccion" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+                        <label htmlFor="correoUsuario">Correo</label>
+                        <input type="text" className="input-text" name="correoUsuario" id="correoUsuario" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+                        <label htmlFor="telefonoContacto">Telefono</label>
+                        <input type="text" className="input-text" name="telefonoContacto" id="telefonoContacto" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                    </div>
+                    <ProductsCarContainer />
+                </form>
+            </section>
         </div>
     )
 }
-
 export default Checkout
--- src/pages/Producto.jsx
+++ src/pages/Producto.jsx
@@ -18,13 +18,14 @@ const Producto = () => {
   const { id } = useParams();
   const { productData } = useGetProductsByID("products", id)
 
-  return (
+  if(usd)
+  { return (
 
     <div className="product-page">
       <div key={productData.id} className="single-product" >
         <img className="product-imagen" src={`../../src/assets/images/tienda/${productData.img}`} alt="" />
         <div>
-          <div className="titulo__formato titulo-producto"><h1>{productData.nombreProducto}</h1></div>
+        <div className="titulo__formato titulo-producto"><h1>{productData.nombreProducto}</h1></div>
           <div className="product-content">
             <p>{productData.descripcion}</p>
             <span>Precio: {Math.floor(productData.valor / usd)} USD - ${productData.valor} CLP. </span>
@@ -34,10 +35,13 @@ const Producto = () => {
           </div>
         </div>
       </div>
-
-
     </div>
-  )
+  )}else{
+   return ( <div className="product-page">
+   <h3>Cargando Datos...</h3>
+   <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+</div>)
+  }
 }
 
 export default Producto
